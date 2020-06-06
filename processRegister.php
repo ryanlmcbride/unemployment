705d861011105bcd5a0f902d1f84b8b5dc4fc055
Rxchmondd <richmond.quar@gmail.com>
2020-06-06 12:16:04 -0400

    process register complete

--- processRegister.php
+++ processRegister.php
@@ -3,9 +3,8 @@
   if(isset($_POST['submit']))
   {
     $SSN = trim($_POST['soc_sec_id']);
-    $fullSSN = $SSN . trim($_POST['ssn2']);
     $DOB = trim($_POST['dob']);
-    $gender =($_POST['gender']);
+    $gender = "";
     $fname = trim($_POST['first_name']);
     $lname = trim($_POST['last_name']);
     $email = trim($_POST['email']);
@@ -13,36 +12,61 @@
     $pword = trim($_POST['password']);
     $address = trim($_POST['address']);
     $city = trim($_POST['city']);
-    $stateid=($_POST['stateid']);
+    $stateid = "";
     $zip = trim($_POST['zipcode']);
 
-    //invalid SSN's
-    $notIssued1 = range(237, 246);
-    $notIssued2 = range(587, 699);
-    $notIssued3 = range(750, 772);
-    $notIssued4 = range(900, 999);
-    $notIssued5 = array(000, 666);
-    $notIssuedFull = array_merge($notIssued1, $notIssued2, $notIssued3, $notIssued4, $notIssued5);
-
-    if(empty($fname)||empty($lname)||empty($email)||empty($SSN)||empty($uname)||empty($pword)||empty($address)||empty($city)||empty($stateid)||empty($zip)||empty($DOB))
+    //check if fields are empty
+    if($fname == '')
     {
-      echo "<html><br><strong style='color: red;'>All fields are required</strong><br></html>";
+      echo "<html><br><strong style='color: red;'>You must enter your first name</strong><br></html>";
+      die();
     }
-
-    if(!is_numeric($SSN)&&strlen($SSN)!= 9)
+    if($lname == '')
     {
-      echo "<html><br><strong style='color: red;'>Invalid SSN</strong><br></html>";
+      echo "<html><br><strong style='color: red;'>You must enter your last name</strong><br></html>";
+      die();
     }
-
-    if(strlen($pword) < 8)
+    if($soc == '')
+    {
+      echo "<html><br><strong style='color: red;'>You must enter a social security number.</strong><br></html>";
+      die();
+    }
+    if($email == '')
+    {
+      echo "<html><br><strong style='color: red;'>You must enter your email</strong><br></html>";
+      die();
+    }
+    if($address == '')
+    {
+      echo "<html><br><strong style='color: red;'>You must enter your address</strong><br></html>";
+      die();
+    }
+    if($city == '')
+    {
+      echo "<html><br><strong style='color: red;'>You must enter your city</strong><br></html>";
+      die();
+    }
+    if($zip == '')
+    {
+      echo "<html><br><strong style='color: red;'>You must enter your zipcode</strong><br></html>";
+      die();
+    }
+    if($DOB == '')
+    {
+      echo "<html><br><strong style='color: red;'>You must enter your date of birth</strong><br></html>";
+      die();
+    }
+    if($pword == '')
+    {
+      echo "<html><br><strong style='color: red;'>Your must enter your password</strong><br></html>";
+      die();
+    }
+    if(strlen($pword) <= 8)
     {
       echo "<html><br><strong style='color: red;'>Your password should be at least 8 characters long</strong><br></html>";
+      die();
     }
 
-    if(!preg_match('/[A-Za-z]/', $pword) && !preg_match('/[0-9]/', $pword))
-    {
-      echo "<html><br><strong style='color: red;'>Your password must have letters AND numbers</strong><br></html>";
-    }
 
     //checks which state option was selected
     if($_POST['state'] == "DC")
@@ -52,10 +76,12 @@
     if($_POST['state'] == "MD")
     {
       echo "<html><br><strong style='color: red;'>You are ineligible for DC unemployment</strong><br></html>";
+      die();
     }
     if($_POST['state'] == "VA")
     {
       echo "<html><br><strong style='color: red;'>You are ineligible for DC unemployment</strong><br></html>";
+      die();
     }
 
     //checks which gender option was selected
@@ -73,22 +99,45 @@
     }
 
     //check SSN
-    if(in_array($SSN, $notIssuedFull))
+    $notIssued1 = range(237, 246);
+    $notIssued2 = range(587, 699);
+    $notIssued3 = range(750, 772);
+    $notIssued4 = range(900, 999);
+    $notIssued5 = array(000, 666);
+    $notIssuedFull = array_merge($notIssued1, $notIssued2, $notIssued3, $notIssued4, $notIssued5);
+
+    if(!is_numeric($SSN))
     {
-      echo "<html><br><strong style='color: red;'>Invalid SSN</strong><br></html>";
+      echo "Please enter a valid social security number";
+      die();
     }
-    else{
-      $query = "INSERT into applicants soc_sec_id, first_name, last_name, email, username, dob, address, city, state_id, zipcode, gender
-      values('$fullSSN', '$fname', '$lname', '$email', '$uname', '$DOB', '$address', '$city', '$stateid', '$zip', '$gender')";
-
-      $results = mysqli_query($conn, $query);
-
-      /*
-      if(mysqli_num_rows($results) == 0)
+    else
+    {
+      $substr_soc = substr($SSN, 0, 2);
+      $social_range = range(577,579);
+      if(!in_array($substr_soc, $social_range) || in_array($notIssuedFull))
       {
-        echo "Invalid D.C. SSN";
+        echo "Please enter a valid social security number";
+        die();
       }
-      */
+      else
+      {
+        //process info
+        //$query = "INSERT into applicants (soc_sec_id, first_name, last_name, email, username, dob, address, city, state_id, zipcode, gender)
+        //VALUES('$SSN', '$fname', '$lname', '$email', '$uname', '$DOB', '$address', '$city', '$stateid', '$zip', '$gender')";
+
+        //$results = mysqli_query($conn, $query);
+
+        //process info second method with extra security
+        $query = "INSERT into applicants (soc_sec_id, first_name, last_name, email, username, dob, address, city, state_id, zipcode, gender)
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        $preparedStatement = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($preparedStatement, issssssisss, $SSN, $fname, $lname, $email, $uname, $DOB, $address, $city, $stateid, $zip, $gender);
+        msqli_stmt_execute($preparedStatment);
+
+        header("Location: success.php");
+      }
     }
   }
 ?>
