ae3970854c644ac730c0b9b0045c724dd194f172
Fr0 Man <20lukerulz05@gmail.com>
2020-06-18 19:01:02 -0400

    User Login Re-Design

--- home.php
+++ home.php
@@ -24,24 +24,27 @@
     <?php require('navbar.php');?>
     <strong><p class="jumbotron" style="font-size:40px; color: #ffffff; background-color:#96c3ee; text-align:center; font-family:arial;">Welcome</p></strong>
     <div class="container">
+    <img src="https://www.wistv.com/resizer/AOWzI7GoVCIOUvlAZIQvHSHTezw=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/7XY7CNV5ZJF4PF5TJ7I4GWVJTI.jpg" style="width:925px; height: 200px;">
       <!--<div class="row" id="main">-->
       <form method="POST" align="left">
         <div class="form-group">
-          <div class="row">
-            <div class="col-sm-6">
+          <div class="row"> 
+          <div class="col-sm-1"></div>
+            <div class="col-sm-5">
               <p>
                 Unemployment is defined as the state of someone who doesn’t have a job. It is something society is suffering from right now, especially with the coronavirus (COVID-19), the whole country is under quarantine restrictions and regulations. With the pandemic, many jobs have unfortunately laid off employees due to the stay at home orders, leaving many people without money to adhear to basic necessities.
               </p>
+              
             </div>
+            
             <span class="border-right"></span>
-            <div class="col-sm-4">
-              <p class="form-group" style="text-align: center;"> Unemployment affects everyone, whether you are self-employed or just a regular employee. In the United States, the government uses surveys, census counts...etc to track unemployment.
-              </p>
-              <div class="col-sm-2"></div>
+            <div class="col-sm-5">
+            
+              <p> Unemployment affects everyone, whether you are self-employed or just a regular employee. In the United States, the government uses surveys, census counts...etc to track unemployment. Tracking unemployment is very important because we need to know what we need to fix in out country. Without being able to track unemployment we wouldnt be able to know how many jobs we need to create.</p>
+            </div>
             </div>
             <hr>
             <p>Our goal as the District of Columbia (D.C.) Unemployment Portal is to provide you with a simple way to provide you with unemployment benefits. We want to stress that if you are not from the D.C. area, you will not be eligible for these specific benefits. Please redirect yourself to your state's unemployment portal. Please stay safe during this time, thank you.</p>
-          </div>
       </form>
   </div>
   </div>
